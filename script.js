b4c524b8f0d22ea6e8e185ded7d959ecac7a9bcf
yabhlnjn <115374440+yabhlnjn@users.noreply.github.com>
2023-05-16 01:28:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+(document).ready(function(){
+    
+    $('.search-toggle').click(function(){
+        $('.search-wrapper').toggleClass('show');
+    });
+
+    });
+
+
+
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+}); 
